57052a202c3287265b72911c42449d42d412e5f2
000cat000 <105432434+000cat000@users.noreply.github.com>
2023-07-14 14:57:35 +0800

    Add files via upload

---
+++ Change.java
@@ -0,0 +1,17 @@
+package com.pcsetting.example;
+
+import java.util.Scanner;
+
+public class Change {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str1;
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("");
+		str1 = scanner.nextLine();
+		System.out.println("efg");
+	}
+
+}
+// 不管輸入什麼，結果都是顯示efg
